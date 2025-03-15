a3280b564db1914b16f63ee00b02a606f8d15e3a
Bagus Hardiyanto <bagushardiyanto03@gmail.com>
2025-03-16 00:22:43 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpmod5
+{
+    class HaloGeneric
+    {
+        public void SapaUser<X>(X nama)
+        {
+            Console.WriteLine($"Halo, selamat pagi {nama}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Tpmod5;
+
+internal class Program
+{
+    private static string? nama { get; set; }
+
+    public static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        Console.Write("Siapa nama anda? ");
+        nama = Console.ReadLine()?.ToUpper();
+        halo.SapaUser(nama);
+    }
+}
